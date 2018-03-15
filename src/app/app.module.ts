4bb189f9fe76976ed89df72e545b7f3e919daa50
Shumaha Smith Asencios Pezo <sasencios@belatrix.com>
2018-03-15 10:08:43 -0500

    update add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {
+  MatFormFieldModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule,
+  MatInputModule, MatSelectModule, MatOptionModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule, MatInputModule, MatSelectModule, MatOptionModule, MatNativeDateModule, MatDatepickerModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
